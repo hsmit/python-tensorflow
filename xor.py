7b1488d0902230e66980ad8c48ff8d68245f96fa
Hannes Smit <hannes.c.smit@gmail.com>
2017-12-09 23:57:28 +0100

    random initialisation

--- xor.py
+++ xor.py
@@ -76,21 +76,15 @@ b was left out for now
 Example was based on TensorFlow Getting started (https://www.tensorflow.org/get_started/get_started) and youtube: 
 https://www.youtube.com/watch?v=kNPGXgzxoHw
 '''
-
-W1 = tf.Variable([
-    [20., 20.],
-    [-20., -20.],
-], dtype=tf.float32, name = 'Weights')
+W1 = tf.Variable(tf.random_uniform([2,2]), dtype=tf.float32, name = 'Weights')
 x = tf.placeholder(shape=(2,None), dtype=tf.float32, name='input')
-b1 = tf.Variable([[-10.],[30.]], dtype=tf.float32, name = 'bias1')
+b1 = tf.Variable(tf.random_uniform([2, 1]), dtype=tf.float32, name = 'bias1')
 
 
 hidden1 = tf.sigmoid(tf.matmul(W1, x) + b1)
 
-W2 = tf.Variable([
-    [20., 20.],
-], dtype=tf.float32, name = 'Weights2')
-b2 = tf.Variable([[-30.]], dtype=tf.float32, name = 'bias1')
+W2 = tf.Variable(tf.random_uniform([1, 2]), dtype=tf.float32, name = 'Weights2')
+b2 = tf.Variable(tf.random_uniform([1, 1]), dtype=tf.float32, name = 'bias2')
 
 a = tf.sigmoid(tf.matmul(W2, hidden1) + b2)
 
@@ -103,7 +97,7 @@ sess.run(init)
 y = tf.placeholder(shape=(1, None), dtype=tf.float32)
 
 loss = tf.reduce_sum(tf.square(a - y)) # sum of the squares
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+optimizer = tf.train.GradientDescentOptimizer(0.8)
 train = optimizer.minimize(loss)
 
 # training data
@@ -118,12 +112,13 @@ y_train = [
 # training loop
 init = tf.global_variables_initializer()
 sess.run(init) # reset values to wrong
-for i in range(1000):
+for i in range(10000):
     sess.run(train, {x: x_train, y: y_train})
 
 # evaluate training accuracy
-curr_W1, curr_W2, curr_b1, curr_b2, curr_loss = sess.run([W1, W2, b1, b2, loss], {x: x_train, y: y_train})
-print("\nW: %s \nb: %s \nloss: %s" % (curr_W1, curr_b1, curr_loss))
+curr_W1, curr_W2, curr_b1, curr_b2, curr_loss = sess.run(
+    [W1, W2, b1, b2, loss], {x: [[1],[0]], y:[[1]]})
+print("\nW1: %s \nW2: %s \nb1: %s \nb2: %s \nloss: %s" % (curr_W1, curr_W2, curr_b1, curr_b2, curr_loss))
 
 
 
