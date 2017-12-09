d206df1c34fef298f129cb16ef4a0203cc2b50a9
Hannes Smit <hannes.c.smit@gmail.com>
2017-12-10 00:02:55 +0100

    small improvement

--- xor.py
+++ xor.py
@@ -24,60 +24,12 @@ O----0
 1 hidden layer with 2 neurons
 1 output layer with 1 output-neuron
 
-The formula I used: a = W*x + b
-
-shape a = (1,1)
-shape W = (1,2)
-shape x = (2,1)
-
-bv:
-
-W = [
-    [20, 20], 
-    [-20, -20]
-]
-x = [
-    [1], 
-    [0]
-]
-
-
-b = [
-    [-10],
-    [30]
-]
-
-dan is a:
-
-a = [
-    [20x1 + 20x0 - 10],
-    [-20x1 - 20x0 + 30]
-]
-=
-[
-[10],
-[10]
-]
-
-
-
-These values can be normalised with an activation function.
-
-
-
-
-
-
-b was left out for now
-
-
-
 
 Example was based on TensorFlow Getting started (https://www.tensorflow.org/get_started/get_started) and youtube: 
 https://www.youtube.com/watch?v=kNPGXgzxoHw
 '''
 W1 = tf.Variable(tf.random_uniform([2,2]), dtype=tf.float32, name = 'Weights')
-x = tf.placeholder(shape=(2,None), dtype=tf.float32, name='input')
+x = tf.placeholder(shape=(2, None), dtype=tf.float32, name='input')
 b1 = tf.Variable(tf.random_uniform([2, 1]), dtype=tf.float32, name = 'bias1')
 
 
@@ -94,7 +46,7 @@ a = tf.sigmoid(tf.matmul(W2, hidden1) + b2)
 init = tf.global_variables_initializer()
 sess.run(init)
 
-y = tf.placeholder(shape=(1, None), dtype=tf.float32)
+y = tf.placeholder(shape=(1, None), dtype=tf.float32, name = 'label')
 
 loss = tf.reduce_sum(tf.square(a - y)) # sum of the squares
 optimizer = tf.train.GradientDescentOptimizer(0.8)
@@ -112,13 +64,14 @@ y_train = [
 # training loop
 init = tf.global_variables_initializer()
 sess.run(init) # reset values to wrong
-for i in range(10000):
+for i in range(50000):
     sess.run(train, {x: x_train, y: y_train})
 
 # evaluate training accuracy
 curr_W1, curr_W2, curr_b1, curr_b2, curr_loss = sess.run(
-    [W1, W2, b1, b2, loss], {x: [[1],[0]], y:[[1]]})
+    [W1, W2, b1, b2, loss], {x: x_train, y:y_train})
 print("\nW1: %s \nW2: %s \nb1: %s \nb2: %s \nloss: %s" % (curr_W1, curr_W2, curr_b1, curr_b2, curr_loss))
 
+# result is now 6e-05
 
 
